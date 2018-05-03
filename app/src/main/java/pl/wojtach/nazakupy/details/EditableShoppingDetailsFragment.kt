38d4ea71d90cf2a0b3953660a409425dc835e33f
Lukasz Wojtach <lukasz.wojtach@allegro.pl>
2018-05-03 22:37:56 +0200

    Pass header id to details fragment

--- app/src/main/java/pl/wojtach/nazakupy/details/EditableShoppingDetailsFragment.kt
+++ app/src/main/java/pl/wojtach/nazakupy/details/EditableShoppingDetailsFragment.kt
@@ -9,34 +9,26 @@ import android.view.ViewGroup
 
 import pl.wojtach.nazakupy.R
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val HEADER_ID_KEY = "header id key"
 
 class EditableShoppingDetailsFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shopping_details, container, false)
-    }
+                              savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.fragment_shopping_details, container, false)
 
 
     companion object {
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(headerId: Long) =
                 EditableShoppingDetailsFragment().apply {
                     arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
+                        putLong(HEADER_ID_KEY, headerId)
                     }
                 }
     }
